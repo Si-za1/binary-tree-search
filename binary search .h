a5f366a33765cf9d8e3a96c8d3661c2ed23c6b11
Si-za1 <55864155+Si-za1@users.noreply.github.com>
2019-12-26 09:48:26 +0545

    Update binary search .h

--- binary search .h
+++ binary search .h
@@ -1,1 +1,9 @@
-#include<iostream>
+#ifndef BinaryTree_h
+#define BinaryTree_h
+class BinaryTree 
+{
+  public:
+    virtual void preorder Traversal()=0;
+    virtual void add (int data) =0;
+    virtual bool search(int data)=0;
+}
