32bad83d72bc5cb2e29d1d2597e3000afd1dd860
Si-za1 <55864155+Si-za1@users.noreply.github.com>
2020-01-08 13:46:35 +0545

    final

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "array.h"
+#include"binary search.h"
+#include <iostream>
+int main()
+{
+	ArrayBST t;
+	t.add(3);
+	t.add(4);
+	t.add(6);
+	t.add(10);
+	t.preorder traversal();
+	
+	 for(int i=0;i<MAXSIZE;i++)
+	 std::cout<<t.element[i];
+}
+ std::cout<<std::endl;
+ a.preorder Traversal(10);
+ if(t.search(6))
+ {
+ 	std::cout<<"6 is present"<<std::endl;
+ 	
+ } 
+   else 
+   {
+   	std::cout<<"6 is absent"<<std::endl;
+   	
+   }
+	
+}
