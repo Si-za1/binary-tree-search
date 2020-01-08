4793ede56b556273874e58284744c4c5e14935d2
Si-za1 <55864155+Si-za1@users.noreply.github.com>
2020-01-08 13:17:23 +0545

    array.h

--- array.h
+++ array.h
@@ -1,8 +1,13 @@
-class array BST
-{
-	private :
-		int elements[16];
+#include "binarysearchtree.h"
+#define MAXSIZE 16
+class ArrayBST:public binaryseacrhtree{
 	public:
-	 void preorder Traversal();
-	 bool add (int data);
-}
+	ArrayBST();
+	~ArrayBST();
+	void preorder Traversal(int index);
+	void add(int data);
+	bool search(int data);
+	int element[MAXSIZE];
+	int get_left_child(int index);
+	int get_right_child(int index);
+};
