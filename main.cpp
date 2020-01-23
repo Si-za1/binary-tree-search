1efd87542cb7a81b129e0d9fa1940f3c726a48fc
Si-za1 <55864155+Si-za1@users.noreply.github.com>
2020-01-23 08:34:54 +0545

    Updated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include"bstl.h"
-#include"binary_search_tree.h"
+#include"binarysearchtree.h"
 #include <iostream>
 using namespace std;
 
@@ -16,5 +16,25 @@ int main() {
     }
     
 
+ArrayBST a;
+	a.add(15);
+	a.add(14);
+	a.add(10);
+	a.add(6);
 
+	
+	for(int i=0; i<MAXSIZE;i++){
+		std::cout<<a.element[i];
+	}
+	std::cout<<std::endl;
+	
+	a.preorderTraversal(10);
+	
+    if(a.search(12)){
+        std::cout<<" 12 in BST"<<std::endl;
+    }
+    else{
+        std::cout<<"12 not present in BST"<<std::endl;
+    }
 
+}
