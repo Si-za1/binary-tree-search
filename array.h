7905d8295afc02002b9b2879dd95cc8837c437dd
Si-za1 <55864155+Si-za1@users.noreply.github.com>
2020-03-05 22:18:29 +0545

    updated

--- array.h
+++ array.h
@@ -1,14 +1,25 @@
 #include "binarysearchtree.h"
-#define MAXSIZE 16
-class ArrayBST:public binaryseacrhtree{
+#define MAX_SIZE 100
+
+class ArrayBST: public BinarySearchTree{
+	
 	public:
-	ArrayBST();
-	~ArrayBST();
-	void preorder Traversal(int index);
-	void add(int data);
-	bool search(int data);
-	int element[MAXSIZE];
-	int get_left_child(int index);
-	int get_right_child(int index);
-	int min(int index);
+		ArrayBST();
+		~ArrayBST();
+		void preorderTraversal();
+		void preorderTraversal(int index);
+		void inorderTraversal();
+		void inorderTraversal(int index);
+		void add(int data);
+		bool search(int data);
+		int get_left_child(int index);
+		int get_right_child(int index);
+		int max();
+		int max(int index);
+		
+		int element[MAX_SIZE];
+		int min(int index);
+		int min();
+		void delete_node();
+		void delete_node(int index,int key);
 };
